f6def4b0100d3bcefb511bed9e2953fd73b1fb55
AndikaHilmiHamdani <andikahilmi7201@gmail.com>
2022-08-07 14:45:58 +0700

    live_chart_frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
+
+const config: SocketIoConfig = {
+	url: environment.socketUrl, // socket server url;
+	options: {
+		transports: ['websocket']
+	}
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config), 
   ],
   providers: [],
   bootstrap: [AppComponent]
