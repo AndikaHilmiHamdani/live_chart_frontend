e7282797c97d76d274660e032025eed131cc7418
AndikaHilmiHamdani <andikahilmi7201@gmail.com>
2022-11-18 00:39:24 +0700

    jwt done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import Chart from 'chart.js/auto';
+import { TokenStorageService } from './_services/token-storage.service';
+// import io from 'socket.io-client';
 
-import io from 'socket.io-client';
-
-const socket = io('ws://localhost:3000');
+// const socket = io('ws://localhost:3000');
 
 @Component({
   selector: 'app-root',
@@ -11,219 +11,244 @@ const socket = io('ws://localhost:3000');
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'dashboard';
-  chart: any;
-  chart2 = [];
-  pie: any;
-  doughnut: any;
+  // title = 'dashboard';
+  // chart: any;
+  // chart2 = [];
+  // pie: any;
+  // doughnut: any;
 
-  data1 = [];
+  // data1 = [];
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+
+  constructor(private tokenStorageService: TokenStorageService) { }
 
   ngOnInit() {
-    this.chart = new Chart('bar', {
-      type: 'bar',
-      options: {
-        responsive: true,
-      },
-      data: {
-        labels: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
-        datasets: [
-          {
-            type: 'bar',
-            label: 'My First dataset',
-            data: [243, 156, 365, 30, 156, 265, 356, 543],
-            backgroundColor: 'rgba(255,0,255,0.4)',
-            borderColor: 'rgba(255,0,255,0.4)',
-            // fill: false,
-          },
-          // {
-          //   type: 'line',
-          //   label: 'Dataset 2',
-          //   backgroundColor: 'rgba(0,0,255,0.4)',
-          //   borderColor: 'rgba(0,0,255,0.4)',
-          //   data: [
-          //     443, 256, 165, 100, 56, 65, 35, 543
-          //   ],
-          //   fill: true,
-          // },
-          {
-            type: 'bar',
-            label: 'My Second dataset',
-            data: [243, 156, 365, 30, 156, 265, 356, 543].reverse(),
-            backgroundColor: 'rgba(0,0,255,0.4)',
-            borderColor: 'rgba(0,0,255,0.4)',
-          },
-        ],
-      },
-    });
 
-    let options = {
-      // aspectRatio: 1,
-      // legend: false,
-      tooltips: false,
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
 
-      elements: {
-        point: {
-          borderWidth: function (context: any) {
-            return Math.min(Math.max(1, context.datasetIndex + 1), 8);
-          },
-          hoverBackgroundColor: 'transparent',
-          hoverBorderColor: function (context: any) {
-            return 'red';
-          },
-          hoverBorderWidth: function (contex: any) {
-            var value = contex.dataset.data[contex.dataIndex];
-            return Math.round((8 * value.v) / 1000);
-          },
-          radius: function (context: any) {
-            var value = context.dataset.data[context.dataIndex];
-            var size = context.chart.width;
-            var base = Math.abs(value.v) / 1000;
-            return (size / 24) * base;
-          },
-        },
-      },
-    };
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
 
-    new Chart('bubble', {
-      type: 'bubble',
-      options: options,
-      data: {
-        datasets: [
-          {
-            backgroundColor: 'rgba(255,0,0,0.4)',
-            label: 'Name 1',
-            data: [
-              {
-                x: 50,
-                y: 60,
-                v: 200,
-              },
-              {
-                x: 50,
-                y: 80,
-                v: 700,
-              },
-              {
-                x: 80,
-                y: 60,
-                v: 100,
-              },
-              {
-                x: 60,
-                y: 60,
-                v: 500,
-              },
-              {
-                x: 90,
-                y: 80,
-                v: 800,
-              },
-            ],
-          },
-          {
-            backgroundColor: 'rgba(0,255,0,0.4)',
-            label: 'Name 2',
-            data: [
-              {
-                x: 60,
-                y: 20,
-                v: 200,
-              },
-              {
-                x: 55,
-                y: 70,
-                v: 800,
-              },
-              {
-                x: 80,
-                y: 30,
-                v: 500,
-              },
-              {
-                x: 70,
-                y: 40,
-                v: 800,
-              },
-            ],
-          },
-        ],
-      },
-    });
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
 
-    this.doughnut = new Chart('doughnut', {
-      type: 'doughnut',
-      options: {
-        responsive: true,
-        animation: {
-          animateScale: true,
-          animateRotate: true,
-        },
-      },
-      data: {
-        datasets: [
-          {
-            data: [45, 10, 5, 25, 15],
-            backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue'],
-            label: 'Dataset 1',
-          },
-        ],
-        labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
-      },
-    });
+      this.username = user.username;
+    }
 
-    this.pie = new Chart('pie', {
-      type: 'pie',
-      options: {
-        responsive: true,
-        animation: {
-          animateScale: true,
-          animateRotate: true,
-        },
-      },
-      data: {
-        datasets: [
-          {
-            data: [45, 10, 5, 25, 15].reverse(),
-            backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue'],
-            label: 'Dataset 1',
-          },
-        ],
-        labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
-      },
-    });
-    socket.on('data1', (res) => {
-      this.updateChartData(this.chart, res, 0);
-      this.updateChartData(this.doughnut,res.slice(0,5), 0);
-    })
+    // this.chart = new Chart('bar', {
+    //   type: 'bar',
+    //   options: {
+    //     responsive: true,
+    //   },
+    //   data: {
+    //     labels: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+    //     datasets: [
+    //       {
+    //         type: 'bar',
+    //         label: 'My First dataset',
+    //         data: [243, 156, 365, 30, 156, 265, 356, 543],
+    //         backgroundColor: 'rgba(255,0,255,0.4)',
+    //         borderColor: 'rgba(255,0,255,0.4)',
+    //         // fill: false,
+    //       },
+    //       // {
+    //       //   type: 'line',
+    //       //   label: 'Dataset 2',
+    //       //   backgroundColor: 'rgba(0,0,255,0.4)',
+    //       //   borderColor: 'rgba(0,0,255,0.4)',
+    //       //   data: [
+    //       //     443, 256, 165, 100, 56, 65, 35, 543
+    //       //   ],
+    //       //   fill: true,
+    //       // },
+    //       {
+    //         type: 'bar',
+    //         label: 'My Second dataset',
+    //         data: [243, 156, 365, 30, 156, 265, 356, 543].reverse(),
+    //         backgroundColor: 'rgba(0,0,255,0.4)',
+    //         borderColor: 'rgba(0,0,255,0.4)',
+    //       },
+    //     ],
+    //   },
+    // });
 
-    socket.on('data2', (res) => {
-      this.updateChartData(this.chart, res, 1);
-    })
+    // let options = {
+    //   // aspectRatio: 1,
+    //   // legend: false,
+    //   tooltips: false,
+
+    //   elements: {
+    //     point: {
+    //       borderWidth: function (context: any) {
+    //         return Math.min(Math.max(1, context.datasetIndex + 1), 8);
+    //       },
+    //       hoverBackgroundColor: 'transparent',
+    //       hoverBorderColor: function (context: any) {
+    //         return 'red';
+    //       },
+    //       hoverBorderWidth: function (contex: any) {
+    //         var value = contex.dataset.data[contex.dataIndex];
+    //         return Math.round((8 * value.v) / 1000);
+    //       },
+    //       radius: function (context: any) {
+    //         var value = context.dataset.data[context.dataIndex];
+    //         var size = context.chart.width;
+    //         var base = Math.abs(value.v) / 1000;
+    //         return (size / 24) * base;
+    //       },
+    //     },
+    //   },
+    // };
+
+    // new Chart('bubble', {
+    //   type: 'bubble',
+    //   options: options,
+    //   data: {
+    //     datasets: [
+    //       {
+    //         backgroundColor: 'rgba(255,0,0,0.4)',
+    //         label: 'Name 1',
+    //         data: [
+    //           {
+    //             x: 50,
+    //             y: 60,
+    //             v: 200,
+    //           },
+    //           {
+    //             x: 50,
+    //             y: 80,
+    //             v: 700,
+    //           },
+    //           {
+    //             x: 80,
+    //             y: 60,
+    //             v: 100,
+    //           },
+    //           {
+    //             x: 60,
+    //             y: 60,
+    //             v: 500,
+    //           },
+    //           {
+    //             x: 90,
+    //             y: 80,
+    //             v: 800,
+    //           },
+    //         ],
+    //       },
+    //       {
+    //         backgroundColor: 'rgba(0,255,0,0.4)',
+    //         label: 'Name 2',
+    //         data: [
+    //           {
+    //             x: 60,
+    //             y: 20,
+    //             v: 200,
+    //           },
+    //           {
+    //             x: 55,
+    //             y: 70,
+    //             v: 800,
+    //           },
+    //           {
+    //             x: 80,
+    //             y: 30,
+    //             v: 500,
+    //           },
+    //           {
+    //             x: 70,
+    //             y: 40,
+    //             v: 800,
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // });
+
+    // this.doughnut = new Chart('doughnut', {
+    //   type: 'doughnut',
+    //   options: {
+    //     responsive: true,
+    //     animation: {
+    //       animateScale: true,
+    //       animateRotate: true,
+    //     },
+    //   },
+    //   data: {
+    //     datasets: [
+    //       {
+    //         data: [45, 10, 5, 25, 15],
+    //         backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue'],
+    //         label: 'Dataset 1',
+    //       },
+    //     ],
+    //     labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+    //   },
+    // });
+
+    // this.pie = new Chart('pie', {
+    //   type: 'pie',
+    //   options: {
+    //     responsive: true,
+    //     animation: {
+    //       animateScale: true,
+    //       animateRotate: true,
+    //     },
+    //   },
+    //   data: {
+    //     datasets: [
+    //       {
+    //         data: [45, 10, 5, 25, 15].reverse(),
+    //         backgroundColor: ['red', 'orange', 'yellow', 'green', 'blue'],
+    //         label: 'Dataset 1',
+    //       },
+    //     ],
+    //     labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+    //   },
+    // });
+    // socket.on('data1', (res) => {
+    //   this.updateChartData(this.chart, res, 0);
+    //   this.updateChartData(this.doughnut,res.slice(0,5), 0);
+    // })
+
+    // socket.on('data2', (res) => {
+    //   this.updateChartData(this.chart, res, 1);
+    // })
   }
 
-  addData(chart: any, label: any, data: any) {
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset: any) => {
-      dataset.data.push(data);
-    });
-    chart.update();
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
   }
+  
+  // addData(chart: any, label: any, data: any) {
+  //   chart.data.labels.push(label);
+  //   chart.data.datasets.forEach((dataset: any) => {
+  //     dataset.data.push(data);
+  //   });
+  //   chart.update();
+  // }
 
-  removeData(chart: any) {
-    chart.data.labels.pop();
-    chart.data.datasets.forEach((dataset: any) => {
-      dataset.data.pop();
-    });
-    chart.update();
-  }
+  // removeData(chart: any) {
+  //   chart.data.labels.pop();
+  //   chart.data.datasets.forEach((dataset: any) => {
+  //     dataset.data.pop();
+  //   });
+  //   chart.update();
+  // }
 
-  updateChartData(chart: any, data: any, dataSetIndex: any) {
-    console.log(chart);
-    console.log(dataSetIndex);
-    chart.data.datasets[dataSetIndex].data = data;
-    chart.update();
-  }
+  // updateChartData(chart: any, data: any, dataSetIndex: any) {
+  //   console.log(chart);
+  //   console.log(dataSetIndex);
+  //   chart.data.datasets[dataSetIndex].data = data;
+  //   chart.update();
+  // }
 
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+
+import { authInterceptorProviders } from './_helper/auth.interceptor';
 const config: SocketIoConfig = {
 	url: environment.socketUrl, // socket server url;
 	options: {
@@ -13,13 +25,23 @@ const config: SocketIoConfig = {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
     SocketIoModule.forRoot(config), 
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
